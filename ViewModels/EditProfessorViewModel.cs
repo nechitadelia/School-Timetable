3f5b67c6c0cb38d2157b206a99fd670982efad81
nchdelia <nechita.delia9@gmail.com>
2024-04-24 15:49:39 +0300

    Validation for Edit Professor

--- ViewModels/EditProfessorViewModel.cs
+++ ViewModels/EditProfessorViewModel.cs
@@ -1,11 +1,23 @@
 ﻿using School_Timetable.Models;
+using School_Timetable.Utilities;
+using System.ComponentModel.DataAnnotations;
 
 namespace School_Timetable.ViewModels
 {
 	public class EditProfessorViewModel
 	{
 		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Please enter the first name")]
+		[MinLength(2, ErrorMessage = "The name must have at least 2 letters")]
+		[MaxLength(100, ErrorMessage = "The name must have maximum 100 letters")]
+		[AllowedOnlyLetters(ErrorMessage = "The name must contain only letters")]
 		public string FirstName { get; set; }
+
+		[Required(ErrorMessage = "Please enter the last name")]
+		[MinLength(2, ErrorMessage = "The name must have at least 2 letters")]
+		[MaxLength(50, ErrorMessage = "The name must have maximum 50 letters")]
+		[AllowedOnlyLetters(ErrorMessage = "The name must contain only letters")]
 		public string LastName { get; set; }
 		public SchoolSubject ProfessorSubject { get; set; }
 		public int AssignedHours { get; set; }
